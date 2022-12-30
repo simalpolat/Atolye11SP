b010274ca9d4f10ce0d916b0aebd8e3dedcb9624
Şimal <115520852+simalpolat@users.noreply.github.com>
2022-12-30 23:40:40 +0300

    Hangman
    Atölye performans ödevim

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Adam_asmaca_vol_5
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.button1.Location = new System.Drawing.Point(647, 220);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(176, 54);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "OYUNA BAŞLA";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.button2.Location = new System.Drawing.Point(571, 333);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(163, 69);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "KELİME T.";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click_1);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.button3.Location = new System.Drawing.Point(740, 333);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(163, 69);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "HARF TAHMİN";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click_1);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(939, 312);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(119, 29);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Can sayısı:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(1064, 312);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(26, 29);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "*";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.textBox1.Location = new System.Drawing.Point(608, 141);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(251, 44);
+			this.textBox1.TabIndex = 6;
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 23;
+			this.listBox1.Location = new System.Drawing.Point(39, 365);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(331, 50);
+			this.listBox1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Adam_asmaca_vol_5.Properties.Resources._1;
+			this.pictureBox1.Location = new System.Drawing.Point(39, 14);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(331, 327);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+			this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(1163, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 23);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "label1";
+			this.label1.Visible = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(1230, 469);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.Margin = new System.Windows.Forms.Padding(5);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load_1);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_asmaca_vol_5
+{
+	public partial class Form1 : Form
+	{
+
+		Random randm = new Random();
+		string[] kelimeler = new string[] { "KÖPEK", "TAPİR", "LAGOSE", "BABİL", "BİZON", "TIRTIL", "BOĞA", "SİVRİSİNEK ", "YENGEÇ", "TAVUK" };
+		public string randomkelime;
+		public int rastgele;
+		public string[] harf;
+		public int imageNumber = 1;
+		public int kalancan;
+	    public string secilenkelime;
+
+		//hocam kodu yazarken  Çınar Çok   ve  Yavuzdan yardım aldım ama kodu tamamen kendim yazdım. Söz veriyorum daha çok çalışacağım
+	    // nice seneler <3
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load_1(object sender, EventArgs e)
+		{
+			pictureBox1.Image = imageList1.Images[imageNumber];
+			//hocam burada aldığım hatayu bir türlü çözemedim :/ 
+			// başlangıç değerim ile alaklı olduğnunu düşünüyordum ama değişsem de olmadı.
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+
+			listBox1.Items.Clear();
+			rastgele = randm.Next(0, kelimeler.Length);
+			randomkelime = kelimeler[rastgele];
+			randomkelime = randomkelime.ToUpper();
+			listBox1.Items.Add(randomkelime);
+			harf = new string[randomkelime.Length];
+			kalancan = 3;
+			label2.Text = " ";
+			label3.Text = kalancan.ToString();
+			for (int a = 0; a < randomkelime.Length; a++)
+			{
+				harf[a] = "_";
+				label2.Text += harf[a] + " ";
+			}
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			string secilenkelime = textBox1.Text.ToUpper();
+
+			if (secilenkelime == randomkelime)
+			{
+				for (int i = 0; i < randomkelime.Length; i++)
+				{
+					harf[i] = secilenkelime[i].ToString();
+				}
+			}
+
+			else
+			{
+				kalancan--;
+				imageNumber++;
+				pictureBox1.Image = imageList1.Images[imageNumber];
+
+				if (kalancan == 0)
+				{
+					MessageBox.Show("Üzgünüm kaybettiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					label2.Text = "";
+				}
+			}
+			if (randomkelime == secilenkelime)
+			{
+				MessageBox.Show("Terkler kazandınız!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				label2.Text = "";
+			}
+
+			label3.Text = kalancan.ToString();
+			label2.Text = " ";
+
+			for (int k = 0; k < randomkelime.Length; k++)
+			{
+				label2.Text += harf[k] + " ";
+			}
+		}
+          
+
+		private void button3_Click_1(object sender, EventArgs e)
+		{
+
+			string secilenkelime = textBox1.Text.ToUpper();
+			bool döngü = false;
+
+			for (int i = 0; i < randomkelime.Length; i++)
+			{
+				if (secilenkelime[0] == randomkelime[i])
+				{
+					harf[i] = secilenkelime[0].ToString();
+					döngü = true;
+				}
+			}
+
+			if (döngü == false)
+			{
+				kalancan--;
+				imageNumber++;
+				pictureBox1.Image = imageList1.Images[imageNumber];
+			}
+			label3.Text = kalancan.ToString();
+
+	    	if (kalancan == 0)
+			{
+				MessageBox.Show("Üzgünüm kaybettiniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				label2.Text = "";
+			}
+
+			else if (randomkelime == secilenkelime)
+			{
+				MessageBox.Show("Tebrikler kazandınız!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				label2.Text = "";
+			}
+
+			label2.Text = " ";
+			for (int j = 0; j < randomkelime.Length; j++)
+			{
+				label2.Text += harf[j] + " ";
+			}
+		}
+
+	}
+}
